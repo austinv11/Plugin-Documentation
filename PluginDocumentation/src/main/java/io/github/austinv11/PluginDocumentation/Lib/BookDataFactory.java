d776f0dfb177c9a6f5834da8bddf8891abbd3a17
austinv11 <austinv11@gmail.com>
2014-08-10 15:40:12 -0400

    (hopefully) Added support for sections

--- PluginDocumentation/src/main/java/io/github/austinv11/PluginDocumentation/Lib/BookDataFactory.java
+++ PluginDocumentation/src/main/java/io/github/austinv11/PluginDocumentation/Lib/BookDataFactory.java
@@ -1,5 +1,6 @@
 package io.github.austinv11.PluginDocumentation.Lib;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -56,10 +57,44 @@ public class BookDataFactory {
 	}
 	
 	public static List<BookData> newBookDataList(String plugin) throws Exception{
+		List<BookData> data = new ArrayList<BookData>();
 		String version;
 		JSONObject json = JSONUtils.listToJSON(URLUtils.readGithub(plugin.toUpperCase()+"/index.json"));
 		version = (String) json.get("Version");
-		
-		return null;
+		JSONArray array = (JSONArray) json.get("Sections");
+		for (int iterator = 0; iterator < array.size(); iterator++){
+			JSONObject sectionJSON = JSONUtils.listToJSON(URLUtils.readGithub(plugin.toUpperCase()+array.get(iterator)+"/index.json"));
+			//Modified, but taken from newBookData()
+			int chapters = (int) sectionJSON.get("Chapters");
+			HashMap<Integer, List<String>> contents = null;
+			boolean startAtOne = false;
+			if (!URLUtils.URLReader("https://raw.github.com/austinv11/Plugin-Documentation/master/"+plugin.toUpperCase()+"/"+array.get(iterator)+"/Chatpter0.txt").contains("Not Found")){
+				startAtOne = true;
+			}
+			int key = 0;
+			for (int i = 0; i < chapters; i++){
+				key = i;
+				if (startAtOne){
+					key++;
+				}
+				contents.put(key, URLUtils.readGithub(plugin.toUpperCase()+"/"+array.get(iterator)+"/Chapter"+key+".txt"));
+			}
+			if ((boolean) sectionJSON.get("HasLinks")){
+				HashMap<String,String> links = null;
+				JSONArray sectionArray = (JSONArray) sectionJSON.get("Links");
+				sectionArray.iterator();
+				for (int j = 0; j < sectionArray.size(); j++){
+					JSONObject linkJSON = (JSONObject) sectionArray.get(j);
+					links.put((String)linkJSON.get("Title"), (String)linkJSON.get("URL"));
+				}
+				if (Resources.CONFG.getBoolean("InternalCaching")){
+					linkCache.put((String)array.get(iterator), links);
+				}
+				data.add(new BookData(true,links,(String) array.get(iterator),version,chapters,"PluginDoumentation",contents));
+			}else{
+				data.add(new BookData(false,null,(String) array.get(iterator),version,chapters,"PluginDoumentation",contents));
+			}
+		}
+		return data;
 	}
 }
