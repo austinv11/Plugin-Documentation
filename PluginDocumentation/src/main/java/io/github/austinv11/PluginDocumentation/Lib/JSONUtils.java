3ba147e84686badca9f4d17bd708cc37e1eb5519
austinv11 <austinv11@gmail.com>
2014-08-10 22:14:28 -0400

    Moar fix attempts

--- PluginDocumentation/src/main/java/io/github/austinv11/PluginDocumentation/Lib/JSONUtils.java
+++ PluginDocumentation/src/main/java/io/github/austinv11/PluginDocumentation/Lib/JSONUtils.java
@@ -3,6 +3,7 @@ package io.github.austinv11.PluginDocumentation.Lib;
 import java.util.List;
 
 import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 import org.json.simple.parser.JSONParser;
 
 public class JSONUtils {
@@ -12,7 +13,8 @@ public class JSONUtils {
 		for (int i = 0; i < list.size(); i++){
 			json = json+list.get(i);
 		}
-		JSONParser parser = new JSONParser();
-		return (JSONObject) parser.parse(json);
+		/*JSONParser parser = new JSONParser();
+		return (JSONObject) parser.parse(json);*/
+		return (JSONObject) JSONValue.parse(json);
 	}
 }
